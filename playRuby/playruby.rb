88f4cbadda57fe4c143855212637a157c528b5a0
elizlalala <eliz.marie.brown@gmail.com>
2016-03-03 18:46:30 +0100

    Adds 4.2.1 Defining Variables rb file and my playRuby sandbox for studying

---
+++ playRuby/playruby.rb
@@ -0,0 +1,20 @@
+#I know your whole name
+
+# puts 'What is your first name?'
+
+# firstName = gets.chomp
+
+# puts 'What is your last name?'
+
+# lastName = gets.chomp
+
+# puts 'What is your middle name?'
+
+# middleName = gets.chomp
+
+# puts 'So you\'re the ' + firstName + ' ' + middleName + ' ' + lastName + ' ' + 'everyone\'s been talking about around here!!'
+
+#what does puts return
+
+returnVal = puts 'This puts returned:'
+# puts returnVal
