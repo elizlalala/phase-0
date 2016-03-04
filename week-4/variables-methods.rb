3f44f8cff9d66a5cf3a99e70456ef5a066a13235
elizlalala <eliz.marie.brown@gmail.com>
2016-03-04 18:50:40 +0100

    Adds completed 4.3 reflection

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -29,3 +29,40 @@ faveNumber = Integer(gets.chomp)
 faveNumber = faveNumber + 1
 
 puts 'Hmm, I suggest changing your favorite number to ' + faveNumber.to_s + ' because it is a much bigger and better favorite number.'
+
+=begin
+	
+How do you define a local variable?
+
+Local Variables have meaning within the construct that they are defined. 
+Constructs include but are not limited to loops, methods, classes, or an entire programs. 
+Local variables must be declared (or initialized) within 
+their intended construct before you start putting them to work. 
+Local variables in more granular constructs will not affect local variables 
+of the same name in a more general construct.
+
+How do you define a method?
+
+Methods execute a defined set of actions that can be called upon 
+again and again throughout your program using a bit of assigned text.  
+Some methods have a required set of parameters that must be given 
+when the method is called.
+
+What is the difference between a local variable and a method?
+
+A local variable stores a piece inforamtion.  
+A method stores and performs actions.
+
+How do you run a ruby program from the command line?
+
+Type in: ruby <filename>
+
+How do you run an RSpec file from the command line?
+
+Type in: rb <filename>
+
+What was confusing about this material? What made sense?
+
+The trickiest thing was toggling between integers and strings in the favorite number method.
+	
+=end
