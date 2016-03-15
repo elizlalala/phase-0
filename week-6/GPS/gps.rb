f27e98281fb3d824908ef9e81440c8cb96bf80b4
elizlalala <eliz.marie.brown@gmail.com>
2016-03-15 16:38:00 +0100

    Adds reflections to GPS 2.3

--- week-6/GPS/gps.rb
+++ week-6/GPS/gps.rb
@@ -50,3 +50,27 @@ p serving_size_calc("cake", 12)
 # p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
+
+# What did you learn about making code readable by working on this challenge?
+# => Making code readable goes beyond comments and indentation.  It extends to the methods and loops
+# => that you choose to accomplish the intended task.  While the original code worked, it was so far
+# => from the most intuitive way to accomplish the tasks at hand.  Following standard best practices
+# => and intuitive coding patterns makes it easier for someone to contribute later.  I guess if you 
+# => wanted to hide the function of your program, you could learn to choose the most unintuitive 
+# => approaches possible.  An artform all on it's own I suppose! :P
+
+# Did you learn any new methods? What did you learn about them?
+# => I had never implemented .has_key? before.  Also, I had never used ArgumentError.new syntax to use
+# => dynamic placeholders in the string.
+
+# What did you learn about accessing data in hashes? 
+# => The only thing I kind of learned about accessing data in hashes was that we saved library[item_to_make] to serving_size
+# => even though it wasn't necessary.  Probably shouldn't make this a habit, but "serving_size" is much more
+# => readable in later operations than library[item_to_make].  If the syntax hash[key] doesn't describe the value
+# => extremely well, and it is going to be used again and again in more complicated lines of code, this could
+# => be a good practice to adopt.  Downside of this is that it bogs your program down with unnecessary variables though.
+
+# What concepts were solidified when working through this challenge?
+# => I liked our use of "unless" here.  It seems like a simple command, but it actually throws me off when I
+# => consider using it.  So I liked solidifying that it is just the equivalent of "if not".
+
