55016e9532130ad0fd53c28a00207070f6b67a4d
elizlalala <eliz.marie.brown@gmail.com>
2016-03-21 12:45:21 +0100

    Catches up all week 6 work so far across multiple modules

---
+++ week-6/6.4-Attr/my_solution.rb
@@ -0,0 +1,42 @@
+#Attr Methods
+
+# I worked on this challenge [by myself]
+
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+class NameData
+
+end
+
+
+class Greetings
+
+end
+
+
+
+# Reflection
+=begin
+
+Release 1:
+Q: What are these methods doing?
+A: They help the user maintain a directory of individuals.  The methods enable you to recall or modify the contents of the directory.
+
+Q: How are they modifying or returning the value of instance variables?
+A: They modify or return the value of the instance variables via an explicitly defined method on command by the user.
+
+Release 2:
+Q: What changed between the last release and this release?
+A: 
+
+Q: What was replaced?
+
+Q: Is this code simpler than the last?
+
+=end
